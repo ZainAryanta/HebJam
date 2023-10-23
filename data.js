1122fc59278200501c3043524c572d4b3f90f86e
ZainAryanta <sowak.exerose@gmail.com>
2023-10-23 21:32:43 +0700

    [Bab 3]State dan Props

---
+++ data.js
@@ -0,0 +1,129 @@
+export const CategoryList = [
+    {
+      id: 1,
+      categoryName: 'Home',
+    },
+    {
+      id: 2,
+      categoryName: 'Library',
+    },
+    {
+      id: 3,
+      categoryName: 'Comunity',
+    },
+    {
+      id: 4,
+      categoryName: 'News',
+    },
+  ];
+  export const BlogList = [
+    {
+    id: 1,
+    title: 'Rediscovering Vinyl: The Resurgence of Analog',
+    category: 'News',
+    image:
+        'https://i.pinimg.com/564x/d9/f2/2c/d9f22ca2536e9df00612b2c22c3f5cfb.jpg',
+    createdAt: 'Nov 10, 2023',
+    totalComments: 89,
+    },
+    {
+      id: 2,
+      title: 'Rediscovering Vinyl: The Resurgence of Analog',
+      category: 'News',
+      image:
+          'https://i.pinimg.com/236x/42/55/98/425598ded8a69b8e44bd91ce6fc06222.jpg',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 3,
+      title: 'Pengobatan Herbal yang selalu mengalami kemunduran',
+      category: 'News',
+      image:
+          'https://i.pinimg.com/236x/40/07/1e/40071e0a47cfc6be029d4f1d4fec7554.jpg',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 4,
+      title: 'Generasi muda yang tertarik dengan obat herbal',
+      category: 'News',
+      image:
+          'https://i.pinimg.com/236x/d3/bd/79/d3bd79a423aa4a786e5cce7dfd7c1512.jpg',
+      createdAt: 'Nov 10, 2023',
+      totalComments: 89,
+    },
+
+
+    {
+      id: 5,
+      title: 'Rediscovering Vinyl: The Resurgence of Analog',
+      category: 'Herbal',
+      image:
+          'https://i.pinimg.com/236x/8d/dd/f9/8dddf97b873fe324eade3c38d177ec4e.jpg',
+      totalComments: 89,
+    },
+    {
+      id: 6,
+      title: 'How to use Redux in ReactJS',
+      category: 'Herbal',
+      image:
+         'https://i.pinimg.com/564x/47/49/1e/47491e303049b04c5b484e09b8bb8150.jpg',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 7,
+      title: 'Boosting Traffic with SEO',
+      category: 'Herbal',
+      image:
+          'https://i.pinimg.com/236x/17/ac/6f/17ac6f011a83811708617e0dc6d1f72d.jpg',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 8,
+      title: 'Culinary Adventures: Exploring Exotic Flavors',
+      category: 'Herbal',
+      image:
+          'https://i.pinimg.com/564x/ae/85/0c/ae850ca669cb3ce3b05b4cc4116f8439.jpg',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 9,
+      title: 'Sneaker Culture',
+      category: 'Herbal',
+      image:
+          'https://i.pinimg.com/236x/b1/5c/71/b15c71cc745782c8752e355fa730b78a.jpg',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 10,
+      title: 'Balancing Work and Well-being',
+      category: 'Lifestyle',
+      image:
+        'https://images.unsplash.com/photo-1602192509154-0b900ee1f851?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 11,
+      title: 'Home Fitness Revolution',
+      category: 'Health',
+      image:
+        'https://images.unsplash.com/photo-1540497077202-7c8a3999166f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+    {
+      id: 12,
+      title: 'Intersection of Fashion',
+      category: 'Fashion',
+      image:
+        'https://images.unsplash.com/photo-1539109136881-3be0616acf4b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80',
+      createdAt: 'Jul 25, 2023',
+      totalComments: 89,
+    },
+  ];
---
+++ src/components/ListHorizontal.js
@@ -0,0 +1,118 @@
+import React, {useState} from 'react';
+import {ScrollView, StyleSheet, Text, View, FlatList,Image,ImageBackground, TouchableOpacity} from 'react-native';
+import {Like1, Send, MessageText1, ProfileCircle, Receipt21,Clock, Message, Add} from 'iconsax-react-native';
+import {BlogList, CategoryList} from './data';
+import FastImage from 'react-native-fast-image';
+import { fontType, colors } from '../theme';
+
+const ItemHorizontal = ({item}) => {
+  return (
+        <View style={styles.listCategory}>
+          <View style={styles.header}>
+            
+          </View>
+          <View style={{...itemHorizontal.cardItem, marginLeft: 20}}>
+            <FastImage
+              style={itemHorizontal.cardImage}
+              resizeMode={FastImage.resizeMode.cover}
+              source={{
+                uri: item.image,
+                headers: {Authorization: 'someAuthToken'},
+                priority: FastImage.priority.high,
+              }}>
+              <View style={itemHorizontal.cardContent}>
+                <View style={itemHorizontal.cardInfo}>
+                 <View style={{backgroundColor:colors.black(0.4), padding:5, borderRadius:15}}>
+                 <Text style={itemHorizontal.cardTitle}>
+                    {item.title}
+                  </Text>
+                  <Text style={itemHorizontal.cardText}>{item.createdAt}</Text>
+                 </View>
+                 
+                </View>
+                <View>
+                  <View style={itemHorizontal.cardIcon}>
+                    <Receipt21 color={colors.white()} variant="Linear" size={20} />
+                  </View>
+                </View>
+              </View>
+            </FastImage>
+          </View>
+
+         
+        </View>
+    
+  );
+};
+const ListHorizontal = ({data}) => {
+  const renderItem = ({item}) => {
+    return (
+      <ItemHorizontal
+        item={item} />
+    );
+  };
+  
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={item => item.id}
+      renderItem={item => renderItem({...item})}
+      ItemSeparatorComponent={() => <View style={{width: 15}} />}
+      contentContainerStyle={{paddingHorizontal: 24}}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+    />
+  );
+};
+
+export default ListHorizontal;
+
+const itemHorizontal = StyleSheet.create({
+    cardItem: {
+      width: 280,
+    },
+    cardImage: {
+      width: '100%',
+      height: 250,
+      borderRadius: 5,
+    },
+    cardContent: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      padding: 0,
+    },
+    cardInfo: {
+      justifyContent: 'flex-end',
+      height: '100%',
+      gap: 10,
+      width:'100%',
+    },
+    cardTitle: {
+      fontSize: 14,
+      color: colors.white(),
+  
+    },
+    cardText: {
+      fontSize: 14,
+      color: colors.white(0.9),
+    },
+    cardIcon: {
+      backgroundColor: colors.white(0.33),
+      padding: 5,
+      borderColor: colors.white(),
+      borderWidth: 0.5,
+      borderRadius: 5,
+      opacity:0
+    },
+  });
+
+  const styles = StyleSheet.create({
+    listCategory: {
+      paddingVertical: 4,
+    },
+
+    header: {
+      marginVertical: 4,
+    }
+  
+  });
